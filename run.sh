f2f6de25dd5817d48a0a553d14318b1a4b2f7078
devensiv <devensiv@gmail.com>
2023-04-19 18:11:46 +0200

    fix: default container name

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 mkdir workdir -p
 
-podman run -it -v ./workdir:/root/workdir -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/dri/card0:/dev/dri/card0 -v ~/.Xauthority:/tmp/.Xauthority -e XAUTHORITY=/tmp/.Xauthority --net=host --name=$1 -e DISPLAY ros2-humble bash
+if [ -z "$1" ]; then
+	name="ros2-default"
+else
+	name="$1"
+fi
+
+podman run -it -v ./workdir:/root/workdir -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/dri/card0:/dev/dri/card0 -v ~/.Xauthority:/tmp/.Xauthority -e XAUTHORITY=/tmp/.Xauthority --net=host --name="$name" -e DISPLAY ros2-humble bash
